7466c2db45e5e822b5f3757d633718f9b6d38623
debbyyeh <app2984@gmail.com>
2025-05-11 00:31:27 +0800

    add footer component

---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+    theme: {
+      extend: {
+        colors: {
+          primary: "#6d4a2f",
+          secondary: "#c3a76c",
+        },
+      },
+    },
+    plugins: [],
+  };
